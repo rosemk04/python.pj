9d427c0a714ea5015d0ffabff8ea4162ea7f2be5
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-04 16:10:05 +0530

    Add files via upload

---
+++ Rose.py
@@ -0,0 +1,54 @@
+'''print('Hello \n World')
+if 5>2:
+   print('Five is greater than two')
+if 5>2:
+print('Five is greater than two')
+if 5>2:
+    print('Five is greater than two')
+if 5>2:
+        print'(Five is greater than two')'''
+
+
+'''x=str(3)
+y=int(3)
+z=float(3)
+print(x,y,z)'''
+'''x=5
+y="John"
+print(type(x))
+print(type(y))'''
+'''
+x,y,z="Orange","Banana","Cherry"
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+'''
+'''
+x=y=z= "Orange"
+print(x)
+print(y)
+print(z)
+'''
+'''x="python"
+y="is"
+z="awesome"
+print(x+y+z)'''
+'''
+username=input("enter username:")
+print("Username is:",username)'''
+'''
+a="Hello"
+b="World"
+c=a+b
+print(c)'''
+
+a="Hello, World"
+print(a[0])
+print(a[1])
+print(a[6])
+
+b="Hello" "World"
+print(b[2:5])
+print(b[:5])
+
