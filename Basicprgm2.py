3c72397619149e86f7041fa2d4a962f5c0facf65
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:09:41 +0530

    Basicprgm2.py

--- Basicprgm2.py
+++ Basicprgm2.py
@@ -1,3 +1,10 @@
+'''
+Author: Rose Mary
+Date:18-10-24
+Discription:program that demonstrates the usage of arithmetic,
+comparison, and logical operators.
+Perform a few operations and print the results.
+'''
 str_var="Hello, World"
 str_another_var=(str_var[7:12])
 print(str_another_var)
