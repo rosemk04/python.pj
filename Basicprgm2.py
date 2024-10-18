cf3e18ebb2580369a780f7432f50f454b7bfc57f
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-18 15:10:32 +0530

    Basicprgm2.py

--- Basicprgm2.py
+++ Basicprgm2.py
@@ -1,9 +1,9 @@
 '''
 Author: Rose Mary
 Date:18-10-24
-Discription:program that demonstrates the usage of arithmetic,
-comparison, and logical operators.
-Perform a few operations and print the results.
+Discription:program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+Finally, display the new string.
 '''
 str_var="Hello, World"
 str_another_var=(str_var[7:12])
