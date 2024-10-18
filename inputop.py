e1e9721cdeb002496f7720ba2a8b09a0fab492cc
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-18 14:34:52 +0530

    Add files via upload

---
+++ inputop.py
@@ -0,0 +1,12 @@
+'''Author: Rose MAry
+Date: 18-10-24
+Discription: program that asks the user to input their name and age.
+After receiving the input, display a message that includes
+the user's name and age.
+ Hello, John! You are 25 years old.
+ '''
+name=input('enter your name:')
+age=int (input('Enter your age:'))
+print("Hello",name,"! You are",age,"years old")
+
+
