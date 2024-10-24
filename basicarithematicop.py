a5519a6b5a405ec5556e40720bcbf92d84cc74fa
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-25 00:02:55 +0530

    Add files via upload

---
+++ basicarithematicop.py
@@ -0,0 +1,14 @@
+'''
+Author: Rose Mary
+Date:18-10-24
+Discription:Simple desktop calculator using Python. Only the five basic arithmetic operators.
+'''
+number1=int(input("Enter num 1: "))
+number2=int(input("Enter num 2: "))
+number3=int(input("Enter  num 3: "))
+print("The sum of num 1 and num 2 is: ",number1+number2)
+print("The difference when num 1 is subtracted from num 2 is: ",number1-number2)
+print("The product of num 1 and num2 is: ",number1*number2)
+print("The quotient when num 1 is divided by num 2 is: ",number1/number2)
+print("The reminder when num 1 is divided by num 2 is: ",number1%number2)
+print("The result of num 1+num 2)*num 3/2 is: ",(number1+number2)*number3/2)
