627e54ddd21e5ead592c5e875cd58812c9fd7648
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-25 15:12:01 +0530

    lrgstno.py

--- lrgstno.py
+++ lrgstno.py
@@ -10,7 +10,7 @@ num1=int(input("Enter first number "))
 num2=int(input("Enter the second number "))
 num3=int(input("Enter the third number"))
 if(num1>num2 and num3<num1):
-    print(num3,"is greater")
+    print(num1,"is greater")
 elif num2>num1 and num2>num3:
     print(num2,"is greater")
 else:
