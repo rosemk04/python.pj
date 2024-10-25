0a0b5d597732ae967b2c8cc9e861459ba310d841
rosemk04 <rosemarymathew2028@cs.sjcetpalai.ac.in>
2024-10-25 14:52:28 +0530

    Add files via upload

---
+++ lrgstno.py
@@ -0,0 +1,17 @@
+'''
+Author: Rose Mary Mathew
+Date:25/10/2024
+Discription:program to find the largest of three numbers.
+The program should take three numbers as input from the user and determine which one is the largest.
+Use conditional statements to compare the numbers.
+'''
+
+num1=int(input("Enter first number "))
+num2=int(input("Enter the second number "))
+num3=int(input("Enter the third number"))
+if(num1>num2 and num3<num1):
+    print(num3,"is greater")
+elif num2>num1 and num2>num3:
+    print(num2,"is greater")
+else:
+    print(num3,"is greater")
